e3f611500cedccc1b4e9c2c3402eb8c270bbd627
djacquila <acquila.santos@gmail.com>
2023-02-14 20:52:55 -0300

    Implementing other endpoints

--- src/main/java/org/trustmd/resources/TrustMd.java
+++ src/main/java/org/trustmd/resources/TrustMd.java
@@ -1,6 +1,5 @@
 package org.trustmd.resources;
 
-import lombok.SneakyThrows;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.eclipse.microprofile.metrics.annotation.Timed;
@@ -8,28 +7,15 @@ import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.info.Info;
 import org.hyperledger.fabric.gateway.*;
-import org.hyperledger.fabric.sdk.Enrollment;
-import org.hyperledger.fabric.sdk.NetworkConfig;
-import org.hyperledger.fabric_ca.sdk.EnrollmentRequest;
-import org.hyperledger.fabric.sdk.security.CryptoSuite;
-import org.hyperledger.fabric.sdk.security.CryptoSuiteFactory;
-import org.hyperledger.fabric_ca.sdk.HFCAClient;
 import org.trustmd.model.Trust;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.Properties;
-
 
 @javax.ws.rs.Path("/trustmd")
 @ApplicationScoped
@@ -40,13 +26,6 @@ public class TrustMd {
     private final static Path ROOT_PATH = Paths.get(CURRENT_HOME_PATH.toString(), "workspace", "minifab", "trustmdtest");
     private final static Path CONNECTIONS_PATH = Paths.get(ROOT_PATH.toString(), "Connections");
     private final static String CONNECTIONS_FILE = "/v1-3p-connection.json";
-
-    private static final String ORGNAME_ORG1 = "org1.example.com";
-    private static final String ADMINNAME_ORG1 = "admin";
-    private static final String ADMINPWD_ORG1 = "adminpw";
-    private static final String CA_CERT_ORG1 = "profiles/" + ORGNAME_ORG1 + "/tls/" + "ca.org1.example.com-cert.pem";
-    private static final String MSPID_ORG1 = "org1-example-com";
-
 
     static {
         System.setProperty("org.hyperledger.fabric.sdk.service_discovery.as_localhost", "true");
@@ -61,33 +40,100 @@ public class TrustMd {
     @Operation(summary = "Add a new Trust info to the ledger", description = "Requires a unique key to be successfully executed")
     public Response createTrust(Trust aTrust) {
         try {
-            _logger.info("Getting wallet");
-            final Path walletPath = Paths.get(ROOT_PATH.toString(), "vars", "app", "go", "wallets");
-            final Wallet wallet = Wallets.newFileSystemWallet(walletPath);
-
-            // Loading connections config file from expected path
-            final Path networkConfigPath = Paths.get(CONNECTIONS_PATH + CONNECTIONS_FILE);
-            _logger.info("Network config path: " + networkConfigPath);
-
-            System.out.println("createTrust with: " + aTrust.toJSONString());
-            // expecting wallet directory within the default server location
-            // wallet exported from Fabric wallets
-
-            final Gateway.Builder gBuilder = Gateway.createBuilder()
-                    .identity(wallet, "Admin")
-                    .networkConfig(networkConfigPath)
-                    .discovery(false);
+            System.out.println("Calling CreateTrust");
+            final Gateway.Builder gBuilder = getGatewayBuilder();
 
             try (final Gateway gateway = gBuilder.connect()) {
-                final Network network = gateway.getNetwork("mychannel");
-                final Contract contract = network.getContract("trustmd");
+                final Contract contract = getContract(gateway);
                 final byte[] result = contract.submitTransaction("createTrust",
                         aTrust.getClusterHeadId(),
                         aTrust.getEvaluatedNodeId(),
                         aTrust.getTrustDecision(),
                         aTrust.getDegreeOfBelief(),
                         aTrust.getMecHostId());
-                return Response.status(Response.Status.OK).entity(Arrays.toString(result)).build();
+                return Response.status(Response.Status.OK).entity(new String(result)).build();
+            } catch (Exception exception) {
+                throw new javax.ws.rs.ServerErrorException("Unable to get network/contract and execute query", Response.Status.INTERNAL_SERVER_ERROR);
+            }
+        } catch (Exception exception) {
+            // Initially raise ServiceUnavailable
+            throw new javax.ws.rs.ServerErrorException("Unable to find config or wallet", Response.Status.NOT_FOUND);
+        }
+    }
+
+    @Timed(name = "updateTrustTime", tags = {
+            "method=put" }, absolute = true, description = "Time needed to update a trust value to the ledger")
+    @PUT
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @javax.ws.rs.Path("/update")
+    @Operation(summary = "Update a new Trust info to the ledger", description = "Requires a unique key to be successfully executed")
+    public Response updateTrust(final Trust aTrust) {
+        try {
+            System.out.println("Calling UpdateTrust");
+            final Gateway.Builder gBuilder = getGatewayBuilder();
+
+            try (final Gateway gateway = gBuilder.connect()) {
+                final Contract contract = getContract(gateway);
+                final byte[] result = contract.submitTransaction("updateTrust",
+                        aTrust.getClusterHeadId(),
+                        aTrust.getEvaluatedNodeId(),
+                        aTrust.getTrustDecision(),
+                        aTrust.getDegreeOfBelief(),
+                        aTrust.getMecHostId());
+                return Response.status(Response.Status.OK).entity(new String(result)).build();
+            } catch (Exception exception) {
+                throw new javax.ws.rs.ServerErrorException("Unable to get network/contract and execute query", Response.Status.INTERNAL_SERVER_ERROR);
+            }
+        } catch (Exception exception) {
+            // Initially raise ServiceUnavailable
+            throw new javax.ws.rs.ServerErrorException("Unable to find config or wallet", Response.Status.NOT_FOUND);
+        }
+    }
+
+    @Timed(name = "getTrustTime", tags = {
+            "method=get" }, absolute = true, description = "Time needed to get a trust value of the ledger")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @javax.ws.rs.Path("{id}")
+    @Operation(summary = "Get the Trust info from the ledger", description = "Requires a unique key to be successfully executed")
+    public Response getTrustByNodeId(@PathParam("id") String evaluatedNodeId) {
+        try {
+            System.out.println("Calling GetTrust " + evaluatedNodeId);
+            final Gateway.Builder gBuilder = getGatewayBuilder();
+
+            try (final Gateway gateway = gBuilder.connect()) {
+                final Contract contract = getContract(gateway);
+                final byte[] result = contract.submitTransaction("getTrust", evaluatedNodeId);
+
+                return Response.status(Response.Status.OK).entity(new String(result)).build();
+            } catch (Exception exception) {
+                throw new javax.ws.rs.ServerErrorException("Unable to get network/contract and execute query", Response.Status.INTERNAL_SERVER_ERROR);
+            }
+        } catch (Exception exception) {
+            // Initially raise ServiceUnavailable
+            throw new javax.ws.rs.ServerErrorException("Unable to find config or wallet", Response.Status.NOT_FOUND);
+        }
+    }
+
+    @Timed(name = "deleteTrustTime", tags = {
+            "method=delete" }, absolute = true, description = "Time needed to get a trust value of the ledger")
+    @DELETE
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @javax.ws.rs.Path("{id}")
+    @Operation(summary = "Delete Trust info from the ledger", description = "Requires a unique key to be successfully executed")
+    public Response deleteByNodeId(@PathParam("id") String evaluatedNodeId) {
+        try {
+            System.out.println("Calling DeleteTrust " + evaluatedNodeId);
+            final Gateway.Builder gBuilder = getGatewayBuilder();
+
+            try (final Gateway gateway = gBuilder.connect()) {
+                final Contract contract = getContract(gateway);
+                final byte[] result = contract.submitTransaction("delete", evaluatedNodeId);
+
+                return Response.status(Response.Status.OK).entity(new String(result)).build();
             } catch (Exception exception) {
                 throw new javax.ws.rs.ServerErrorException("Unable to get network/contract and execute query", Response.Status.INTERNAL_SERVER_ERROR);
             }
@@ -100,10 +146,25 @@ public class TrustMd {
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     @javax.ws.rs.Path("/walletTest")
-    public Response get(Trust aTrust) throws IOException {
-        final Path walletPath = Paths.get(ROOT_PATH.toString(), "vars", "app", "go", "wallets");
-        final Wallet wallet = Wallets.newFileSystemWallet(walletPath);
+    public Response walletTest(Trust aTrust) throws IOException {
+        // expecting wallet directory within the default server location
+        // wallet exported from Fabric wallets
+        final Gateway.Builder gBuilder = getGatewayBuilder();
+        try (final Gateway gateway = gBuilder.connect()) {
+            final Contract contract = getContract(gateway);
+            System.out.println(contract);
+        }
 
+        return Response.status(Response.Status.OK).entity("OK").build();
+    }
+
+    private Wallet getWallet() throws IOException {
+        final Path walletPath = Paths.get(ROOT_PATH.toString(), "vars", "app", "go", "wallets");
+        return Wallets.newFileSystemWallet(walletPath);
+    }
+
+    private Gateway.Builder getGatewayBuilder() throws IOException {
+        final Wallet wallet = getWallet();
         final Identity adminIdentity = wallet.get("Admin");
         if (adminIdentity != null) {
             System.out.println("Entity adminIdentity already registered");
@@ -113,23 +174,14 @@ public class TrustMd {
         final Path networkConfigPath = Paths.get(CONNECTIONS_PATH + CONNECTIONS_FILE);
         System.out.println("Network config path: " + networkConfigPath);
 
-        // expecting wallet directory within the default server location
-        // wallet exported from Fabric wallets
-        try {
-            final Gateway.Builder gBuilder = Gateway.createBuilder()
-                    .identity(wallet, "Admin")
-                    .networkConfig(networkConfigPath)
-                    .discovery(true);
-            try (final Gateway gateway = gBuilder.connect()) {
-                final Network network = gateway.getNetwork("mychannel");
-                final Contract contract = network.getContract("trustmd");
-                System.out.println(contract);
-            }
-
-        } catch (Exception exception) {
-            throw new javax.ws.rs.ServerErrorException("Unable to get network/contract and execute query", Response.Status.INTERNAL_SERVER_ERROR);
-        }
-        return Response.status(Response.Status.OK).entity("OK").build();
+        return Gateway.createBuilder()
+                .identity(wallet, "Admin")
+                .networkConfig(networkConfigPath)
+                .discovery(false);
     }
 
+    private Contract getContract(final Gateway gateway) {
+        final Network network = gateway.getNetwork("mychannel");
+        return network.getContract("trustmd");
+    }
 }
